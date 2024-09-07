b72a218d5f90fc9de264d693ee2ac32d8b84fffc
Pratik2805github <deshmukhpratik850@gmail.com>
2024-09-07 23:55:39 +0530

    specing

--- Git Demo Project/src/com/pratik/Git2024.java
+++ Git Demo Project/src/com/pratik/Git2024.java
@@ -3,5 +3,7 @@ package com.pratik;
 public class Git2024 {
 public static void main(String[] args) {
 	System.out.println("my name is pratik");
+	System.out.println("deshmukh");
+	//add a comment   
 }
 }
