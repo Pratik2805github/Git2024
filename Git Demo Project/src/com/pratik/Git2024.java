dbabc0dee66fbe6b0c6e953419589827f7bdd4b5
Pratik2805github <deshmukhpratik850@gmail.com>
2024-09-08 16:31:32 +0530

    feacher branch

--- Git Demo Project/src/com/pratik/Git2024.java
+++ Git Demo Project/src/com/pratik/Git2024.java
@@ -2,9 +2,6 @@ package com.pratik;
 
 public class Git2024 {
 public static void main(String[] args) {
-	System.out.println("my name is pratik");
-	System.out.println("deshmukh");
-	//add a comment   
-	//double comment
+	System.out.println("This is feacher branch");
 }
 }
